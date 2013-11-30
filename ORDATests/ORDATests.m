65d0c75df935b95bb74ae17d5b0450425d7be59c
Ethan Reesor <ethan.reesor@firelizzard.com>
2013-11-29 23:44:32 -0500

    Setting up CI

---
+++ ORDATests/ORDATests.m
@@ -0,0 +1,29 @@
+//
+//  ORDATests.m
+//  ORDATests
+//
+//  Created by Ethan Reesor on 11/29/13.
+//  Copyright (c) 2013 Firelizzard Inventions. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface ORDATests : XCTestCase
+
+@end
+
+@implementation ORDATests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+@end
